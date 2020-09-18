8666c2d232bde82f20221480f0cff891937030eb
Tprezioso <tommyprezioso@gmail.com>
2020-09-18 14:45:44 -0400

    Initial Commit

---
+++ SideBarBlog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideBarBlog
+//
+//  Created by Thomas Prezioso on 9/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideBarBlog/SideBarBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideBarBlogApp.swift
+//  SideBarBlog
+//
+//  Created by Thomas Prezioso on 9/18/20.
+//
+
+import SwiftUI
+
+@main
+struct SideBarBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
